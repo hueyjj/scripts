745f292287550d6923b55b59ee4056f9b0e1ee7f
Huey <jasperjeng@gmail.com>
2018-03-06 13:02:49 -0800

    Added description to script

---
+++ numlines.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""
+This script will iteratively scan a directory and each sub directory, reading
+each file, and counting the total number of lines.
+
+This script will ignore any empty lines, and other directory that should not
+be scanned such as dotfiles and dotdirectories (.node_modules, .git) and
+build/, etc.
+"""
+
+import os
+
+def get_dir_content(path):
+    entries = os.listdir(path)
+        
+
+# Handle command line arguments
+# By default display totals lines, else if verbose then more information
+
+def main():
+    paths_scanned = []
+    entries = os.listdir(path)
+    for entry in entries:
+        while os.path.isdir(entry):
+            entry = os.listdir(path)
+
+
+if __name__ == "__main__":
+    main()
